1633c9d4bb91c923cd9862eaeebb4f04753f0b9a
Colin Edwards <colin@recursivepenguin.com>
2012-08-06 21:45:19 -0500

    Use home up in the reader activity

--- SubscriptionReader/src/com/econify/stagereads/PlayReader.java
+++ SubscriptionReader/src/com/econify/stagereads/PlayReader.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.econify.stagereads.adapters.BookPagerAdapter;
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.epub.EpubReader;
@@ -27,6 +28,8 @@ public class PlayReader extends SherlockFragmentActivity {
             return;
         }
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
 
         String bookName = intent.getStringExtra("book");
@@ -44,5 +47,20 @@ public class PlayReader extends SherlockFragmentActivity {
         }
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This is called when the Home (Up) button is pressed
+                // in the Action Bar.
+                Intent parentActivityIntent = new Intent(this, Main.class);
+                parentActivityIntent.addFlags(
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(parentActivityIntent);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
